472846ade6fd3b8e8a9558292bf419c666002bae
A-Banjo <grace@charitycan.com>
2023-08-24 14:13:41 -0400

    send api call on button click

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import openai
-from flask import Flask
+from flask import Flask, jsonify, request
+from flask_cors import CORS
 from constants import OPENAI_API_KEY
 
 #openai.organization = "YOUR_ORG_ID"
@@ -20,20 +21,21 @@ sentences = "I was just wondering if this works? I'm not sure, but I think my id
 # print(response)
 
 app = Flask(__name__)
+CORS(app)
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
 
-@app.route('/suggestions')
+@app.route('/suggestions', methods=['POST'])
 def retrieve_suggestions():
+    sentences = request.json['sentences']
     response = openai.ChatCompletion.create(
-    model="gpt-3.5-turbo",
-    messages=[
+        model="gpt-3.5-turbo",
+        messages=[
             {"role": "system", "content": "Can you rephrase each of the following sentences where appropriate to sound more confident and professional, retaining as much meaning and original wording as possible? Do not rephrase or change sentences that do not sound unconfident."},
             {"role": "user", "content": sentences},
         ]
     )
     result = response['choices'][0]['message']['content']
-    print(response)
-    return result
+    return jsonify(result)
--- plugin/misogynrammarly.js
+++ plugin/misogynrammarly.js
@@ -1,130 +1,128 @@
-var selectedField = null;
-
-document.addEventListener('focusin', function(event) {
-  if (selectedField == event.target || event.target.id == 'vera-button') {
-    return;
-  }
-
-  if (selectedField != event.target && selectedField != null) {
-    deleteButton();
-  }
-
-  if (event.target.isContentEditable) {
-    console.log(event.target);
-    createButton(event.target);
-    selectedField = event.target;
-  }
-}, true);
+document.addEventListener(
+  "focusin",
+  function (event) {
+    if (event.target.isContentEditable) {
+      console.log(event.target);
+      createButton(event.target);
+    }
+  },
+  true
+);
 
 function checkText(element) {
   // call the API to check the text
 }
 
 function createButton(element) {
-  const button = document.createElement('button');
-  button.classList.add('vera-button');
-  button.innerHTML = 'Check Text';
-
-  // make the button float to the top right of the element
-  const rect = element.getBoundingClientRect();
-  console.log(rect);
-  button.style.top = '5px';
-  button.style.left = `${rect.width - 100}px`;
-  button.style.position = 'absolute';
-
-  button.id = 'vera-button';
-
-  button.onclick = checkText;
+  const button = document.createElement("button");
+  button.innerHTML = "Check";
+  button.onclick = checkText(element);
   element.parentNode.insertBefore(button, element.nextSibling);
 }
 
-function deleteButton() {
-  const button = document.getElementById('vera-button');
-  button.parentNode.removeChild(button);
-}
-
-function update(){ //update the check
-  if (['TEXTAREA', 'INPUT'].includes(document.activeElement.nodeName)) {
+function update() {
+  //update the check
+  if (["TEXTAREA", "INPUT"].includes(document.activeElement.nodeName)) {
     // it is in textarea, input
     checkMisogyny(document.activeElement.value);
-  }
-  else if (document.activeElement.isContentEditable) {
+  } else if (document.activeElement.isContentEditable) {
     // it is in contentEditable element
     checkMisogyny(document.activeElement.innerHTML);
-  }
-  else {
+  } else {
     // not above
   }
 }
 
 //map of words and suggestions
 const map = new Map();
-map.set('sorry', 'replace with a thank you for their accommodation'); //value cannot include full key or will cause issues
-map.set('maybe', 'have some more confidence!');
-map.set('i think', 'you know!');
-map.set('just', 'consider rephrasing');
-map.set('can you please', "a 'please' will suffice!");
-map.set('probably', 'be more certain!');
-map.set('might', 'you can do it!');
-map.set('does that make any sense?', 'it makes sense!');
-map.set('am i making any sense?', 'you are making sense!');
+map.set("sorry", "replace with a thank you for their accommodation"); //value cannot include full key or will cause issues
+map.set("maybe", "have some more confidence!");
+map.set("i think", "you know!");
+map.set("just", "consider rephrasing");
+map.set("can you please", "a 'please' will suffice!");
+map.set("probably", "be more certain!");
+map.set("might", "you can do it!");
+map.set("does that make any sense?", "it makes sense!");
+map.set("am i making any sense?", "you are making sense!");
 map.set("i'm not too sure", "don't doubt yourself!");
-map.set('i was wondering', 'be more direct!');
+map.set("i was wondering", "be more direct!");
 map.set("i'm no expert", "don't underestimate yourself!");
 
 //check for misogyny-conforming language
-function checkMisogyny(text){
+function checkMisogyny(text) {
   console.log(text); //returns the text
-      if(text != null && text != undefined){
-        //remove previous spans
-        var current = '<span class="vera-highlighted"';
-        text = deleteSpans(current, text);
-        current = '</span>';
-        text = deleteSpans(current, text);
+  if (text != null && text != undefined) {
+    //remove previous spans
+    var current = '<span class="vera-highlighted"';
+    text = deleteSpans(current, text);
+    current = "</span>";
+    text = deleteSpans(current, text);
 
-      //detect and highlight key words
-      const keys = map.keys();
-      var innerHTML = text;
-      for(let i = 0; i<map.size; i++){
-          current = keys.next().value;
-           if((text.toLowerCase()).includes(current)){
-              console.log(map.get(current));
-                var index = innerHTML.toLowerCase().indexOf(current);
-                var newIndex = -1;
-                if (index >= 0) {
-                 innerHTML = innerHTML.substring(0,index) + `<span class="vera-highlighted" title="${map.get(current)}">` + innerHTML.substring(index,index+current.length) + "</span>" + innerHTML.substring(index + current.length);
-                 newIndex = innerHTML.toLowerCase().indexOf(current, index);
-                }
-                while (newIndex >= 0) { //find other instances of the word
-                 innerHTML = innerHTML.substring(0,newIndex) + `<span class="vera-highlighted" title="${map.get(current)}">` + innerHTML.substring(newIndex,newIndex+current.length) + "</span>" + innerHTML.substring(newIndex + current.length);
-                 index = newIndex;
-                 newIndex = text.toLowerCase().indexOf(current, index);
-                }
-            }
+    //detect and highlight key words
+    const keys = map.keys();
+    var innerHTML = text;
+    for (let i = 0; i < map.size; i++) {
+      current = keys.next().value;
+      if (text.toLowerCase().includes(current)) {
+        console.log(map.get(current));
+        var index = innerHTML.toLowerCase().indexOf(current);
+        var newIndex = -1;
+        if (index >= 0) {
+          innerHTML =
+            innerHTML.substring(0, index) +
+            `<span class="vera-highlighted" title="${map.get(current)}">` +
+            innerHTML.substring(index, index + current.length) +
+            "</span>" +
+            innerHTML.substring(index + current.length);
+          newIndex = innerHTML.toLowerCase().indexOf(current, index);
+        }
+        while (newIndex >= 0) {
+          //find other instances of the word
+          innerHTML =
+            innerHTML.substring(0, newIndex) +
+            `<span class="vera-highlighted" title="${map.get(current)}">` +
+            innerHTML.substring(newIndex, newIndex + current.length) +
+            "</span>" +
+            innerHTML.substring(newIndex + current.length);
+          index = newIndex;
+          newIndex = text.toLowerCase().indexOf(current, index);
+        }
       }
-      //detect amount of exclamation marks used
-      let exclamationCount = text.split("!");
-      if(exclamationCount.length>3){
-        index = innerHTML.lastIndexOf("!");
-        innerHTML = innerHTML.substring(0,index) + `<span class="vera-highlighted" title="use less exclamation marks to sound more serious and professional">` + "!" + "</span>" + innerHTML.substring(index + 1);
-      }
-      document.activeElement.innerHTML = innerHTML;
-   }
+    }
+    //detect amount of exclamation marks used
+    let exclamationCount = text.split("!");
+    if (exclamationCount.length > 3) {
+      index = innerHTML.lastIndexOf("!");
+      innerHTML =
+        innerHTML.substring(0, index) +
+        `<span class="vera-highlighted" title="use less exclamation marks to sound more serious and professional">` +
+        "!" +
+        "</span>" +
+        innerHTML.substring(index + 1);
+    }
+    document.activeElement.innerHTML = innerHTML;
+  }
 }
 
 //get rid of old spans
-function deleteSpans(current, text){
+function deleteSpans(current, text) {
   var innerHTML = text;
   var i = innerHTML.indexOf(current);
   var newI = -1;
   if (i >= 0) {
-   innerHTML = innerHTML.substring(0,i) + "" + innerHTML.substring(innerHTML.indexOf(">", i)+1);
-   newI = innerHTML.indexOf(current, i);
+    innerHTML =
+      innerHTML.substring(0, i) +
+      "" +
+      innerHTML.substring(innerHTML.indexOf(">", i) + 1);
+    newI = innerHTML.indexOf(current, i);
   }
   while (newI > 0) {
-   innerHTML = innerHTML.substring(0,newI) + "" + innerHTML.substring(innerHTML.indexOf(">", newI)+1);
-   i = newI;
-   newI = innerHTML.indexOf(current, i);
+    innerHTML =
+      innerHTML.substring(0, newI) +
+      "" +
+      innerHTML.substring(innerHTML.indexOf(">", newI) + 1);
+    i = newI;
+    newI = innerHTML.indexOf(current, i);
   }
-   return innerHTML;
+  return innerHTML;
 }
